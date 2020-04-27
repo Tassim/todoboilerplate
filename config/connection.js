60035b70bc84604ae8ace217792d04f65f3f0509
Tassia Shibuya <tassia.shibuya@gmail.com>
2020-04-27 10:54:02 -0700

    updated config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'todos_db', // trailing comma in case someone wants to write code after this
-}).promise(); // make promisse to every route using mysql
+let connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection.connection(process.env.JAWSDB_URL).promise();
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'todos_db', // trailing comma in case someone wants to write code after this
+}).promise(); // make promisse to every route using mysql
+}
 // test connection by running this file
 // connection.connect((err) => {
 //   if (err) throw err;
